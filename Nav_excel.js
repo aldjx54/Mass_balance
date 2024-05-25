cb7c9cc13d72fec70e408d404fc9972dbe9e5dc0
aldjx54 <louis.gauvain@gmail.com>
2024-05-26 01:12:23 +0200

    Add excel export file

--- Nav_excel.js
+++ Nav_excel.js
@@ -1,22 +1,38 @@
-import exceljs from "exceljs/index";
-import saveAs from 'file-saver';
-import GC from "@grapecity/spread-sheets";
+import * as FileSaver from './scripts/file-saver/src/FileSaver.js';
+import ExcelJS from './scripts/exceljs/excel.js';
 
-const workbook = new exceljs.addWorkbook();
+console.log('1');
+
+const workbook = new ExcelJS.Workbook();
 workbook.creator = 'User';
 workbook.created = new Date();
+workbook.modified = new Date();
 
-const sheet = workbook.addWorksheet('First');
+const flightPlan = workbook.addWorksheet('flightPlan', {
+    properties: { tabColor: { argb:'00B050'}}, views: [{state: 'frozen', ySplit:1}]
+});
 
-document.getElementById("export").onclick = function () {
-    var fileName = $("#exportFileName").val();
-    var json = JSON.stringify(workbook.toJSON());
-    workbook.export(function (blob) {
-        // save blob to a file
-        saveAs(blob, fileName);
-    }, function (e) {
-        console.log(e);
-    }, {
-        fileType: GC.Spread.Sheets.FileType.excel
-    });
-}
+flightPlan.columns = [
+    {header: 'MSA', key: 'msa', width: 10, style: {font: {name: 'Cambria', bold: true, underline: true, size: 12 }}},
+    {header: 'ALT', key: 'alt', width: 10, style: {font: {name: 'Cambria', bold: true, underline: true, size: 12 }}},
+    {header: 'TAS', key: 'tas', width: 10, style: {font: {name: 'Cambria', bold: true, underline: true, size: 12 }}},
+]
+
+console.log('2');
+
+document.getElementById('saveFile').addEventListener('click', async function(e) {
+    try {
+        const buffer = await workbook.xlsx.writeBuffer();
+        const fileType = 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;charset=UTF-8';
+        let EXCEL_EXTENSION = '.xlsx';
+        const blob = new Blob([buffer], {type: fileType});
+
+        FileSaver.saveAs(blob, `Excel_Spreadsheet` + EXCEL_EXTENSION);
+    } catch (err) {
+        console.error(err);
+    }
+
+console.log('3');
+});
+
+console.log('4');
